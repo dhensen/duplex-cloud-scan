4d7280ecc6a75de1645809bff73533cc4f6db087
Dino Hensen <dino.hensen@gmail.com>
2018-07-21 12:19:29 +0200

    feat: combine two pdfs. Lol this litteraly took me 5 minutes to write thanks to pdfrw

---
+++ duplex_cloud_scan/combine_pdf.py
@@ -0,0 +1,19 @@
+from pdfrw import PdfReader, PdfWriter
+
+
+def combine_two_pdfs(front, back, output_file):
+    front_pages = PdfReader(front).pages
+    back_pages = PdfReader(back).pages
+    back_pages = reversed(back_pages)
+    combined = [None] * (len(front_pages) + len(front_pages))
+    combined[::2] = front_pages
+    combined[1::2] = back_pages
+
+    writer = PdfWriter(output_file)
+    for page in combined:
+        writer.addpage(page)
+    writer.write()
+
+
+combine_two_pdfs('example_pdfs/front.pdf', 'example_pdfs/back.pdf',
+                 'combined.pdf')
